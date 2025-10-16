28861e526de516d5155f9e765d0b518795bbdd48
Sunwoo Jung <104188728+willjsw@users.noreply.github.com>
2025-10-16 22:21:43 +0900

    chore #9: application-test 프로필 추가 (#10)

--- src/test/java/com/irum/come2us/Come2usApplicationTests.java
+++ src/test/java/com/irum/come2us/Come2usApplicationTests.java
@@ -2,8 +2,10 @@ package com.irum.come2us;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Come2usApplicationTests {
 
     @Test
