046e2c32a906d81d80d6ff08b071d917b5322884
willjsw <sunwoo1137@gmail.com>
2025-10-16 14:12:39 +0900

    chore #1: project init

---
+++ src/main/java/com/irum/come2us/Come2usApplication.java
@@ -0,0 +1,13 @@
+package com.irum.come2us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Come2usApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Come2usApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/irum/come2us/Come2usApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irum.come2us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Come2usApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
