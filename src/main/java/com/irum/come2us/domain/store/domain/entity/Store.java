5a487e6717b7fa197b333fde853a5edbc97fb9b7
isak-kang <wbsldj59@naver.com>
2025-10-17 17:01:12 +0900

    feature #15 : store 도메인 추가

---
+++ src/main/java/com/irum/come2us/domain/store/domain/entity/Store.java
@@ -0,0 +1,51 @@
+package com.irum.come2us.domain.store.domain.entity;
+
+import com.irum.come2us.domain.member.domain.entity.Member;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+
+@Getter
+@Entity
+@Table(name = "p_store")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class Store {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "store_id", updatable = false, nullable = false)
+    private UUID storeId;
+
+
+    @Column(name = "store_name", nullable = false, length = 50)
+    private String storeName;
+
+    @Column(name = "contact", nullable = false, columnDefinition = "char(13)")
+    private String contact;
+
+    @Column(name = "address", nullable = false, length = 50)
+    private String address;
+
+    @Column(name = "business_registration_number", nullable = false, columnDefinition = "char(10)")
+    private String businessRegistrationNumber;
+
+    @Column(name = "telemarketing_registration_number", nullable = false, columnDefinition = "char(10)")
+    private String telemarketingRegistrationNumber;
+
+    @Column(name = "delivery_fee", nullable = false)
+    private int deliveryFee;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false, unique = true)
+    private Member memberId;
+
+}
