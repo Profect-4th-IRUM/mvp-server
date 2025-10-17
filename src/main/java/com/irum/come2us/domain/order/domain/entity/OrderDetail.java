83c31c5ff7029b0154f0137a63e7596b2aae7b07
hyeonji91 <hjkim1770@gmail.com>
2025-10-17 17:05:16 +0900

    feature #5 : order detail 엔티티 작성

---
+++ src/main/java/com/irum/come2us/domain/order/domain/entity/OrderDetail.java
@@ -0,0 +1,50 @@
+package com.irum.come2us.domain.order.domain.entity;
+
+import com.irum.come2us.domain.order.domain.entity.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Builder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="p_order_datail")
+public class OrderDetail {
+    @Id
+    @UuidGenerator
+    @Column(name="order_id", columnDefinition = "uuid", nullable = false, updatable = false)
+    private UUID orderDetailId;
+
+    private String optionName;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private OrderStatus orderStatusIndi;
+
+    private Integer tracking_number;
+
+    private LocalDateTime arrivedDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="order_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Order order;
+}
