16b4106586c9d36213c7c4b46d24c06100b48fa7
hyeonji91 <hjkim1770@gmail.com>
2025-10-17 18:00:56 +0900

    fix  #5: payment entity 어노테이션 붙임

--- src/main/java/com/irum/come2us/domain/payment/domain/entity/Payment.java
+++ src/main/java/com/irum/come2us/domain/payment/domain/entity/Payment.java
@@ -3,9 +3,7 @@ package com.irum.come2us.domain.payment.domain.entity;
 import com.irum.come2us.domain.payment.domain.entity.enums.PaymentCorp;
 import com.irum.come2us.domain.payment.domain.entity.enums.PaymentMethod;
 import com.irum.come2us.domain.payment.domain.entity.enums.PaymentStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -13,6 +11,7 @@ import org.hibernate.annotations.UuidGenerator;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Table(name = "p_payment")
 public class Payment {
     @Id
@@ -20,12 +19,18 @@ public class Payment {
     @Column(name="payment_id", columnDefinition = "uuid", nullable = false, updatable = false)
     private String paymentId;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PaymentMethod paymentMethod;
 
     private Integer amount;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PaymentStatus paymentStatus;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PaymentCorp paymentCorp;
 
     // TODO: member @ManyToOne
