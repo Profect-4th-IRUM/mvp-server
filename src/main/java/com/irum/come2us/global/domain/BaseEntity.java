62ca2e500eed9e7aa24dae275536d5b2729e32af
Sunwoo Jung <104188728+willjsw@users.noreply.github.com>
2025-10-17 12:54:54 +0900

    [Feature] base entity 구현 (#11)
    * feature #7: base entity 구현
    
    * refactor #7: createdBy, updatedBy 필드 String -> Long 타입 변경(Member ID Long 타입)

---
+++ src/main/java/com/irum/come2us/global/domain/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.irum.come2us.global.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false, nullable = false)
+    private Long createdBy;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    private Long updatedBy;
+
+    private LocalDateTime deletedAt;
+}
