d64c775f8ee7d45dedcbe9e9754e3c36373e8aaa
willjsw <sunwoo1137@gmail.com>
2025-10-17 19:10:58 +0900

    feature #22: MemberRepository 및 ClientRepository(Customer, Owner)구현

---
+++ src/main/java/com/irum/come2us/domain/member/domain/repository/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.irum.come2us.domain.member.domain.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends MemberRepository {}
