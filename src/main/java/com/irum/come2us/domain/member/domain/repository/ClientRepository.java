2ef589d7b4e17ee1c95c8825ff3ff2e458b19088
willjsw <sunwoo1137@gmail.com>
2025-10-19 00:12:35 +0900

    refactor #25:기존 ClientRepository -> MemberRepository로 통폐합

--- src/main/java/com/irum/come2us/domain/member/domain/repository/ClientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.irum.come2us.domain.member.domain.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends MemberRepository {}
