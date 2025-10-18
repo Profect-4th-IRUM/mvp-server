9f90c94d5679dd271d6b3d113944fa3b71f90b15
Sunwoo Jung <104188728+willjsw@users.noreply.github.com>
2025-10-19 00:20:53 +0900

    Feature/#25 member manager crud (#51)
    * refactor #25: Transactional import 경로 수정 및 조회 로직 readOnly 옵션 추가
    
    * feature #25: MemberValidator 클래스 생성(검증 유틸 메서드 클래스 분리)
    
    * feature #25: ManagerController 클래스 작성(마스터 권한 매니저 CRUD API 일반 멤버와 분리)
    
    * feature #25: ManagerService 구현
    
    * feature #25: QueryDSL기반 무한스크롤 서치 구현 위한 Custom Repository 구현
    
    * feature #25: MemberInfoListResponse DTO 구현
    
    * refactor #25: MemberService 메서드 네이밍 및 판매자 권한 에러코드 수정, Manager 권한 확인 에러코드 추가
    
    * refactor #25:기존 ClientRepository -> MemberRepository로 통폐합

--- src/main/java/com/irum/come2us/domain/member/application/service/ManagerService.java
+++ src/main/java/com/irum/come2us/domain/member/application/service/ManagerService.java
@@ -6,7 +6,9 @@ import com.irum.come2us.domain.member.domain.repository.MemberRepository;
 import com.irum.come2us.domain.member.presentation.dto.request.MemberCreateRequest;
 import com.irum.come2us.domain.member.presentation.dto.request.MemberInfoUpdateRequest;
 import com.irum.come2us.domain.member.presentation.dto.request.MemberPasswordUpdateRequest;
+import com.irum.come2us.domain.member.presentation.dto.response.MemberInfoListResponse;
 import com.irum.come2us.domain.member.presentation.dto.response.MemberInfoResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,50 +16,60 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class ManagerService {
     private final MemberRepository memberRepository;
     private final MemberValidator memberValidator;
 
-    public void createCustomer(MemberCreateRequest request) {
+    public void createManager(MemberCreateRequest request) {
         memberValidator.assertEmailIsNotTaken(request.email());
         memberRepository.save(
-                Member.createCustomer(
-                        request.email(), request.password(), request.name(), request.contact()));
-    }
-
-    public void createOwner(MemberCreateRequest request) {
-        memberValidator.validateNewOwnerRegistration(request.email());
-        memberRepository.save(
-                Member.createOwner(
+                Member.createManager(
                         request.email(), request.password(), request.name(), request.contact()));
     }
 
     @Transactional(readOnly = true)
-    public MemberInfoResponse findMemberInfo() {
-        Member member = memberValidator.getCurrentMember();
+    public MemberInfoResponse findManagerInfo(Long memberId) {
+        Member member = memberValidator.getMemberById(memberId);
         return MemberInfoResponse.createMemberInfoResponse(member);
     }
 
-    public void changeMemberNameAndContact(MemberInfoUpdateRequest request) {
+    @Transactional(readOnly = true)
+    public MemberInfoListResponse findManagerInfoList(Long lastMemberId, int pageSize) {
         Member member = memberValidator.getCurrentMember();
+        int limit = pageSize + 1;
+        List<MemberInfoResponse> memberInfoList =
+                memberRepository.findMembersByCursor(lastMemberId, limit);
+        boolean hasNext = memberInfoList.size() > pageSize;
+        Long nextCursor = null;
+        List<MemberInfoResponse> responseList = memberInfoList;
+        if (hasNext) {
+            responseList = memberInfoList.subList(0, pageSize);
+            nextCursor = responseList.get(pageSize - 1).memberId();
+        }
+
+        return new MemberInfoListResponse(responseList, nextCursor, hasNext);
+    }
+
+    public void changeManagerNameAndContact(Long memberId, MemberInfoUpdateRequest request) {
+        Member member = memberValidator.getMemberById(memberId);
         member.updateName(request.name());
         member.updateContact(request.contact());
     }
 
-    public void changeMemberPassword(MemberPasswordUpdateRequest request) {
-        Member member = memberValidator.getCurrentMember();
+    public void changeManagerPassword(Long memberId, MemberPasswordUpdateRequest request) {
+        Member member = memberValidator.getMemberById(memberId);
         memberValidator.validatePassword(request.originalPassword(), request.newPassword(), member);
         member.updatePassword(request.newPassword());
     } // 추후 BCryptEncoder 사용한 암/복호화 검증 로직 적용 예정
 
-    public void changeCustomerRoleToOwner() {
-        Member member = memberValidator.getCurrentMember();
+    public void changeManagerRoleToMaster(Long memberId) {
+        Member member = memberValidator.getMemberById(memberId);
         memberValidator.assertMemberIsNotOwner(member);
         member.grantOwner();
     }
 
-    public void withdrawCustomer() {
-        Member member = memberValidator.getCurrentMember();
+    public void removeManager(Long memberId) {
+        Member member = memberValidator.getMemberById(memberId);
         memberValidator.assertMemberIsCustomer(member);
         memberRepository.delete(member);
     }
---
+++ src/main/java/com/irum/come2us/domain/member/application/util/MemberValidator.java
@@ -0,0 +1,66 @@
+package com.irum.come2us.domain.member.application.util;
+
+import com.irum.come2us.domain.member.domain.entity.Member;
+import com.irum.come2us.domain.member.domain.entity.enums.Role;
+import com.irum.come2us.domain.member.domain.repository.MemberRepository;
+import com.irum.come2us.global.presentation.advice.exception.CommonException;
+import com.irum.come2us.global.presentation.advice.exception.errorcode.MemberErrorCode;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MemberValidator {
+    private final MemberRepository memberRepository;
+
+    public Member getCurrentMember() {
+        return memberRepository
+                .findByMemberId(1L)
+                .orElseThrow(() -> new CommonException(MemberErrorCode.MEMBER_NOT_FOUND));
+    } // Spring Security 도입 후 SecurityContextHolder를 통해 검증하도록 변경 예정
+
+    public Member getMemberById(Long memberId) {
+        return memberRepository
+                .findByMemberId(memberId)
+                .orElseThrow(() -> new CommonException(MemberErrorCode.MEMBER_NOT_FOUND));
+    } // Spring Security 도입 후 SecurityContextHolder를 통해 검증하도록 변경 예정
+
+    public void validatePassword(String originalPassword, String newPassword, Member member) {
+        if (member.getPassword().equals(originalPassword))
+            throw new CommonException(MemberErrorCode.INVALID_PASSWORD);
+        if (member.getPassword().equals(newPassword))
+            throw new CommonException(MemberErrorCode.DUPLICATED_PASSWORD);
+    }
+
+    public void validateNewOwnerRegistration(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isPresent()) {
+            Role role = member.get().getRole();
+            switch (role) {
+                case OWNER -> throw new CommonException(MemberErrorCode.MEMBER_ALREADY_EXISTS);
+                case CUSTOMER -> throw new CommonException(MemberErrorCode.OWNER_UPGRADE_REQUIRED);
+            }
+        }
+    }
+
+    public void assertMemberIsNotOwner(Member member) {
+        if (member.getRole().equals(Role.OWNER))
+            throw new CommonException(MemberErrorCode.ROLE_ALREADY_GRANTED);
+    }
+
+    public void assertEmailIsNotTaken(String email) {
+        if (memberRepository.findByEmail(email).isPresent())
+            throw new CommonException(MemberErrorCode.MEMBER_ALREADY_EXISTS);
+    }
+
+    public void assertMemberIsCustomer(Member member) {
+        Role role = member.getRole();
+        if (!role.equals(Role.CUSTOMER)) {
+            switch (role) {
+                case OWNER -> throw new CommonException(MemberErrorCode.OWNER_CANNOT_WITHDRAW);
+                case MANAGER -> throw new CommonException(MemberErrorCode.MANAGER_CANNOT_WITHDRAW);
+            }
+        }
+    }
+}
