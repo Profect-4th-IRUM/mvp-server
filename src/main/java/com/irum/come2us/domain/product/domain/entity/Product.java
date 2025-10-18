a91e6e2af70e002f4daa7b5a2a083064c35de2f4
Bal1oon <ws32159@naver.com>
2025-10-18 18:56:05 +0900

    feature #12: Product 엔티티 작성

---
+++ src/main/java/com/irum/come2us/domain/product/domain/entity/Product.java
@@ -0,0 +1,90 @@
+package com.irum.come2us.domain.product.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "p_product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
+    @Column(name = "product_id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "detail_description", columnDefinition = "TEXT", nullable = false)
+    private String detailDescription;
+
+    @Column(name = "is_public", nullable = false)
+    private boolean isPublic;
+    // enum 생성
+
+    @Column(name = "avg_rating")
+    private Double avgRating;
+
+    @Column(name = "review_count")
+    private Integer reviewCount;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Product(String name,
+                    String description,
+                    boolean isPublic,
+                    String detailDescription,
+                    int price) {
+        this.name = name;
+        this.description = description;
+        this.isPublic = isPublic;
+        this.detailDescription = detailDescription;
+        this.price = price;
+    }
+
+    public static Product createProduct(String name,
+                                        String description,
+                                        String detailDescription,
+                                        int price,
+                                        boolean isPublic) {
+        return Product.builder()
+                .name(name)
+                .description(description)
+                .detailDescription(detailDescription)
+                .price(price)
+                .isPublic(isPublic)
+                .build();
+    }
+
+    public void updateProduct(String name,
+                              String description,
+                              String detailDescription,
+                              int price,
+                              boolean isPublic) {
+        this.name = name;
+        this.description = description;
+        this.detailDescription = detailDescription;
+        this.price = price;
+        this.isPublic = isPublic;
+    }
+
+
+    public void updateRating(Double avgRating, Integer reviewCount) {
+        this.avgRating = avgRating;
+        this.reviewCount = reviewCount;
+    }
+
+    // TODO: Store, Category, 이미지, 옵션 매핑
+}
