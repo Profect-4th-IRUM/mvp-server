ceda2ea050cd80103b4d49c77b9bde27c44c022b
willjsw <sunwoo1137@gmail.com>
2025-10-19 20:52:00 +0900

    refactor #64: member password 검증/생성로직 BCrypt 기반 해싱/비교 적용

--- src/main/java/com/irum/come2us/domain/member/application/util/MemberValidator.java
+++ src/main/java/com/irum/come2us/domain/member/application/util/MemberValidator.java
@@ -11,12 +11,14 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class MemberValidator {
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public Member getCurrentMember() {
         return memberRepository
@@ -46,9 +48,9 @@ public class MemberValidator {
     } // 타 사용자의 정보 조회(MANAGER, MASTER 권한)
 
     public void validatePassword(String originalPassword, String newPassword, Member member) {
-        if (member.getPassword().equals(originalPassword))
+        if (!passwordEncoder.matches(originalPassword, member.getPassword()))
             throw new CommonException(MemberErrorCode.INVALID_PASSWORD);
-        if (member.getPassword().equals(newPassword))
+        if (passwordEncoder.matches(newPassword, member.getPassword()))
             throw new CommonException(MemberErrorCode.DUPLICATED_PASSWORD);
     }
 
