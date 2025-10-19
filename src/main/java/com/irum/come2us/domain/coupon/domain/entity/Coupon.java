924184052483822095a493c6c8c48d1123d25200
goorm <moses@goormui-MacBookPro.local>
2025-10-19 13:23:50 +0900

    쿠폰 엔티티

---
+++ src/main/java/com/irum/come2us/domain/coupon/domain/entity/Coupon.java
@@ -0,0 +1,73 @@
+package com.irum.come2us.domain.coupon.domain.entity;
+
+
+import com.irum.come2us.domain.member.domain.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+// 1. 엔티티
+
+@Entity
+@Table(name = "Coupon")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coupon {
+
+    @Id // 쿠폰 아이디
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
+    @Column(name = "coupon_id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", length = 20) // 쿠폰명
+    private String name;
+
+    @Column(name = "discount_amount") // 할인 금액
+    private Integer discountAmount;
+
+    @Column(name = "member_id") // 회원 식별 아이디
+    private Long memberId;
+
+    @Column(name = "expiration") // 유효기간
+    private LocalDateTime expiration;
+    /*
+    향후 쿠폰 사용 기능 추가시 주석 해제
+
+        @Column(name = "is_used", nullable = false) // 쿠폰 사용 여부
+        private boolean isUsed = false;
+
+        @Column(name = "used_at") // 쿠폰 사용 시점
+        private LocalDateTime usedAt;
+    */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(insertable = false, updatable = false)
+    private Member member;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Coupon(String name, Integer discountAmount, LocalDateTime expiration, Member member) {
+        this.name = name;
+        this.discountAmount = discountAmount;
+        this.expiration = expiration;
+        this.member = member;
+        //  this.isUsed = false;
+    }
+
+    // 2. 쿠폰 생성 정적 팩토리 메서드
+
+    public static Coupon createCoupon(
+            String name, Integer discountAmount, LocalDateTime expiration, Member member) {
+        return Coupon.builder()
+                .name(name)
+                .discountAmount(discountAmount)
+                .expiration(expiration)
+                .member(member)
+                .build();
+
+    }
+}
+
+
+
