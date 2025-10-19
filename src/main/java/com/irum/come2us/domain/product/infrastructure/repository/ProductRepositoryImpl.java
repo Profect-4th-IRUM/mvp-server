7203ab55e0bcbb9418d6b7e176b7c4b5023003db
Bal1oon <ws32159@naver.com>
2025-10-19 20:21:54 +0900

    feature #44: ProductRepository 상품 목록 검색 구현

--- src/main/java/com/irum/come2us/domain/product/infrastructure/repository/ProductRepositoryImpl.java
+++ src/main/java/com/irum/come2us/domain/product/infrastructure/repository/ProductRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.irum.come2us.domain.product.infrastructure.repository;
 import com.irum.come2us.domain.product.domain.entity.QProduct;
 import com.irum.come2us.domain.product.domain.repository.ProductRepositoryCustom;
 import com.irum.come2us.domain.product.presentation.dto.response.ProductResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.UUID;
@@ -13,6 +15,17 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class ProductRepositoryImpl implements ProductRepositoryCustom {
     private final JPAQueryFactory queryFactory;
+
+    private BooleanExpression ltCursor(UUID cursor, QProduct product) {
+        return cursor != null ? product.id.lt(cursor) : null;
+    }
+
+    private BooleanExpression containsKeyword(String keyword, QProduct product) {
+        if (keyword == null || keyword.trim().isEmpty()) {
+            return null;
+        }
+        return product.name.containsIgnoreCase(keyword);
+    }
 
     @Override
     public List<ProductResponse> findProductsByCursor(UUID cursor, int size) {
@@ -29,5 +42,31 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
         }
 
         return query.fetch().stream().map(ProductResponse::from).toList();
+    }
+
+    @Override
+    public List<ProductResponse> findProductsByKeyword(UUID cursor, int size, String keyword) {
+        QProduct product = QProduct.product;
+
+        return queryFactory
+                .select(
+                        Projections.constructor(
+                                ProductResponse.class,
+                                product.id,
+                                product.name,
+                                product.description,
+                                product.detailDescription,
+                                product.price,
+                                product.isPublic,
+                                product.avgRating,
+                                product.reviewCount))
+                .from(product)
+                .where(
+                        product.isPublic.isTrue(),
+                        ltCursor(cursor, product),
+                        containsKeyword(keyword, product))
+                .orderBy(product.id.desc())  // createdAt 기준 정렬로 변경 예정
+                .limit(size)
+                .fetch();
     }
 }
