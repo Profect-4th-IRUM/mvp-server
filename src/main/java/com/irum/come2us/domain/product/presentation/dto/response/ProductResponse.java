3fa7d8718b334c3778b420e4f7534fc216d3d1ad
Bal1oon <ws32159@naver.com>
2025-10-19 16:28:59 +0900

    feature #42: ProductResponse DTO 확장

--- src/main/java/com/irum/come2us/domain/product/presentation/dto/response/ProductResponse.java
+++ src/main/java/com/irum/come2us/domain/product/presentation/dto/response/ProductResponse.java
@@ -3,13 +3,27 @@ package com.irum.come2us.domain.product.presentation.dto.response;
 import com.irum.come2us.domain.product.domain.entity.Product;
 import java.util.UUID;
 
+/**
+ * ProductResponse - 상품 등록/수정/조회 공용 DTO - 추후 ProductListResponse를 분리할지 고민
+ *
+ * @param id
+ * @param name
+ * @param description
+ * @param detailDescription
+ * @param price
+ * @param isPublic
+ * @param avgRating
+ * @param reviewCount
+ */
 public record ProductResponse(
         UUID id,
         String name,
         String description,
         String detailDescription,
         int price,
-        boolean isPublic) {
+        boolean isPublic,
+        Double avgRating,
+        Integer reviewCount) {
     public static ProductResponse from(Product product) {
         return new ProductResponse(
                 product.getId(),
@@ -17,6 +31,8 @@ public record ProductResponse(
                 product.getDescription(),
                 product.getDetailDescription(),
                 product.getPrice(),
-                product.isPublic());
+                product.isPublic(),
+                product.getAvgRating(),
+                product.getReviewCount());
     }
 }
