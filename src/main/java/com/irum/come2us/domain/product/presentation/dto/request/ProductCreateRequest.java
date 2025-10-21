80edd5ae61e34f46e84b7f91fae559fe6830e6b1
Bal1oon <ws32159@naver.com>
2025-10-21 16:10:16 +0900

    feature #122: 상품 옵션 생성 관련 DTO 정의

--- src/main/java/com/irum/come2us/domain/product/presentation/dto/request/ProductCreateRequest.java
+++ src/main/java/com/irum/come2us/domain/product/presentation/dto/request/ProductCreateRequest.java
@@ -3,10 +3,12 @@ package com.irum.come2us.domain.product.presentation.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 
 public record ProductCreateRequest(
         @NotBlank(message = "상품명은 필수 입력값입니다.") String name,
         @NotBlank(message = "상품 설명은 필수 입력값입니다.") String description,
         @NotBlank(message = "상품 상세 설명은 필수 입력값입니다.") String detailDescription,
         @NotNull(message = "상품 공개 여부는 필수 입력값입니다.") Boolean isPublic,
-        @Min(value = 0, message = "상품 가격은 0 이상이어야 합니다.") int price) {}
+        @Min(value = 0, message = "상품 가격은 0 이상이어야 합니다.") int price,
+        List<ProductOptionGroupRequest> optionGroups) {}
