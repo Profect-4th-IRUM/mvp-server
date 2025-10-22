e2381e5b87bb23c49072c0b0b1a0f18e7f9e0a39
isak-kang <wbsldj59@naver.com>
2025-10-22 14:20:41 +0900

    [Feature] Store CRUD 구현 (#105)
    * feature #46: Store 엔티티에 createStore 정적 팩토리 메서드 추가
    
    * feature #46: StoreCreate DTO 구현
    
    * feature #46: StoreRepository 구현
    
    * feature #46: StoreService 구현
    
    * feature #46: StoreController 구현
    
    * feature #46: StoreErrorCode 구현
    
    * refactor #46: Store 도메인에 유효성 검증 로직 추가 및 DTO 검증 책임 재구성
    
    * feature #48: Store 엔티티에 updateStore(이름, 전화번호, 주소, 배달비용) 정적 팩토리 메서드 추가
    
    * feature #48: StoreUpdateRequest 구현
    
    * feature #48: StoreErrorCode 추가
    
    * feature #48: Storeupdate Service 로직구현
    
    * feature #48: StoreUpdate API 구현 Controller
    
    * feature #49: store Soft Delete 구현(@SQLDelete, Where 절 조건 설정)
    
    * feature #49: Store 삭제 구현.
    
    * feature #47: QueryDSL 기반 상점 검색 기능 구현 (Repository, DTO)
    
    * feature #47: 상점 검색 API 추가 (Service, Controller)
    
    * feature #47: 상점 상세정보 검색(DTO, Service, Controller)
    
    * refactor #45: 배송비와 기본 정보 업데이트 로직 분리
    
    * refactor #45 : int-> Integer 수정
    
    * refactor #45 : readOnly 옵션 추가
    
    * refactor #45 : 상점 생성 API post /stores/store-create ->post /stores
    
    * refactor #45 : 배송비 integer -> int / member 권한 validate 주석처리, member와 겹치는 컴파일 값 삭제
    
    * fectuer #45: 본인의 상점만 수정/삭제할 수 있게 수정.
    
    * refactor #45: 상점도메인 베이스엔티티상속
    
    * refactor : SQL 쿼리문 수정
    
    * refactor : 서비스,컨트롤러 메서드 이름 수정 및 검증 추가
    
    * refactor : 수정
    
    ---------
    
    Co-authored-by: Sunwoo Jung <104188728+willjsw@users.noreply.github.com>

---
+++ src/main/java/com/irum/come2us/domain/store/presentation/controller/StoreController.java
@@ -0,0 +1,76 @@
+package com.irum.come2us.domain.store.presentation.controller;
+
+import com.irum.come2us.domain.store.application.service.StoreService;
+import com.irum.come2us.domain.store.presentation.dto.request.StoreCreateRequest;
+import com.irum.come2us.domain.store.presentation.dto.request.StoreDeliveryFeeUpdateRequest;
+import com.irum.come2us.domain.store.presentation.dto.request.StoreUpdateRequest;
+import com.irum.come2us.domain.store.presentation.dto.response.StoreCreateResponse;
+import com.irum.come2us.domain.store.presentation.dto.response.StoreInfoResponse;
+import com.irum.come2us.domain.store.presentation.dto.response.StoreListResponse;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stores")
+@Slf4j
+public class StoreController {
+
+    private final StoreService storeService;
+
+    @PostMapping
+    public ResponseEntity<StoreCreateResponse> createStore(
+            @Valid @RequestBody StoreCreateRequest request) {
+        log.info("상점 생성 요청: {}", request);
+        UUID storeId = storeService.registerStore(request);
+        return ResponseEntity.ok(new StoreCreateResponse(storeId));
+    }
+
+    // TODO: Security 적용
+
+    @PatchMapping("/{storeId}")
+    public ResponseEntity<Void> updateStore(
+            @PathVariable UUID storeId, @Valid @RequestBody StoreUpdateRequest request) {
+        log.info("상점 정보 수정 요청: storeId={}, request={}", storeId, request);
+        storeService.changeStore(storeId, request);
+        return ResponseEntity.noContent().build(); // 204 No Content
+    }
+
+    @PatchMapping("/delivery_fee/{storeId}")
+    public ResponseEntity<Void> updateDeliveryFee(
+            @PathVariable UUID storeId, @Valid @RequestBody StoreDeliveryFeeUpdateRequest request) {
+        log.info("상점 배달비 수정 요청: storeId={}, request={}", storeId, request);
+        storeService.changeDeliveryFee(storeId, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    // TODO: Security 적용
+
+    @DeleteMapping("/{storeId}")
+    public ResponseEntity<Void> deleteStore(@PathVariable UUID storeId) {
+        log.info("상점 삭제 요청 : storeId={}", storeId);
+        storeService.withdrawStore(storeId);
+        return ResponseEntity.noContent().build(); // 204 No Content
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StoreListResponse>> getStoreList(
+            @RequestParam(required = false) UUID cursor,
+            @RequestParam(required = false) Integer size) {
+        log.info("상점 목록 조회 요청: cursor={}, size={}", cursor, size);
+        List<StoreListResponse> response = storeService.findStoreList(cursor, size);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{storeId}")
+    public ResponseEntity<StoreInfoResponse> getStoreDetail(@PathVariable UUID storeId) {
+        log.info("상점 상세 조회 요청: storeId={}", storeId);
+        StoreInfoResponse response = storeService.findStoreInfo(storeId);
+        return ResponseEntity.ok(response);
+    }
+}
