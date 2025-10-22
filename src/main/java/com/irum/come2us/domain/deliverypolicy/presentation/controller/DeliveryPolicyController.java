7d165e169e29ae84b9d5c38513bbbbb310364465
isak-kang <wbsldj59@naver.com>
2025-10-22 19:31:28 +0900

    feacture #141: 배송비 정책 수정 API 구현

--- src/main/java/com/irum/come2us/domain/deliverypolicy/presentation/controller/DeliveryPolicyController.java
+++ src/main/java/com/irum/come2us/domain/deliverypolicy/presentation/controller/DeliveryPolicyController.java
@@ -2,29 +2,36 @@ package com.irum.come2us.domain.deliverypolicy.presentation.controller;
 
 import com.irum.come2us.domain.deliverypolicy.application.service.DeliveryPolicyService;
 import com.irum.come2us.domain.deliverypolicy.presentation.dto.request.DeliveryPolicyCreateRequest;
+import com.irum.come2us.domain.deliverypolicy.presentation.dto.request.DeliveryPolicyInfoUpdateRequest;
 import com.irum.come2us.domain.deliverypolicy.presentation.dto.response.DeliveryPolicyCreateResponse;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/deliver-policy")
+@RequestMapping("/delivery-policies")
 @RequiredArgsConstructor
 @Slf4j
 public class DeliveryPolicyController {
     private final DeliveryPolicyService deliveryPolicyService;
 
     @PostMapping
-    public ResponseEntity<DeliveryPolicyCreateResponse> registerStore(
+    public ResponseEntity<DeliveryPolicyCreateResponse> registerDeliveryPolicy(
             @Valid @RequestBody DeliveryPolicyCreateRequest request) {
-        log.info("판매자 회원가입 요청: {}", request);
+        log.info("배달비 정책 등록: {}", request);
         UUID deliveryPolicyId = deliveryPolicyService.createDeliveryPolicy(request);
         return ResponseEntity.ok(new DeliveryPolicyCreateResponse(deliveryPolicyId));
+    }
+
+    @PatchMapping("/{deliveryPolicyId}")
+    public ResponseEntity<Void> updateDeliveryPolicy(
+            @PathVariable UUID deliveryPolicyId,
+            @Valid @RequestBody DeliveryPolicyInfoUpdateRequest request) {
+        log.info("배달비 정책 수정: deliveryPolicyId={}, request={}", deliveryPolicyId, request);
+        deliveryPolicyService.changeDeliveryPolicy(deliveryPolicyId, request);
+        return ResponseEntity.noContent().build();
     }
 }
