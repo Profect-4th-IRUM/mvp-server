8799a0fd39d72be59ff12bf2e7bd3e6f012e8b6f
isak-kang <wbsldj59@naver.com>
2025-10-22 19:31:04 +0900

    feacture #141: 배송비 정책 수정 서비스 로직 구현

--- src/main/java/com/irum/come2us/domain/deliverypolicy/application/service/DeliveryPolicyService.java
+++ src/main/java/com/irum/come2us/domain/deliverypolicy/application/service/DeliveryPolicyService.java
@@ -3,6 +3,7 @@ package com.irum.come2us.domain.deliverypolicy.application.service;
 import com.irum.come2us.domain.deliverypolicy.domain.entity.DeliveryPolicy;
 import com.irum.come2us.domain.deliverypolicy.domain.repository.DeliveryPolicyRepository;
 import com.irum.come2us.domain.deliverypolicy.presentation.dto.request.DeliveryPolicyCreateRequest;
+import com.irum.come2us.domain.deliverypolicy.presentation.dto.request.DeliveryPolicyInfoUpdateRequest;
 import com.irum.come2us.domain.member.application.util.MemberValidator;
 import com.irum.come2us.domain.member.domain.entity.Member;
 import com.irum.come2us.domain.store.domain.entity.Store;
@@ -26,7 +27,7 @@ public class DeliveryPolicyService {
     public UUID createDeliveryPolicy(DeliveryPolicyCreateRequest request) {
         Member member = memberValidator.getCurrentMember();
         Store store = validateAndGetStoreByMember(member);
-        validateStoreHasNotPolicy(store);
+        ensureStoreHasNoExistingPolicy(store);
 
         DeliveryPolicy deliveryPolicy =
                 DeliveryPolicy.createPolicy(
@@ -38,21 +39,47 @@ public class DeliveryPolicyService {
         return deliveryPolicy.getId();
     }
 
-    private void validateStoreHasNotPolicy(Store store) {
+    public void changeDeliveryPolicy(
+            UUID deliveryPolicyId, DeliveryPolicyInfoUpdateRequest request) {
+        Member member = memberValidator.getCurrentMember();
+        DeliveryPolicy deliveryPolicy = getDeliveryPolicyById(deliveryPolicyId);
+        Store store = validateAndGetStoreByMember(member);
+
+        validateStoreOwner(store, member);
+
+        deliveryPolicy.updateFee(request.defaultDeliveryFee());
+        deliveryPolicy.updateQuantity(request.minQuantity());
+        deliveryPolicy.updateAmount(request.minAmount());
+    }
+
+    // 상점 정책 존재하는지 확인
+    private void ensureStoreHasNoExistingPolicy(Store store) {
         if (deliveryPolicyRepository.existsByStore(store)) {
             throw new CommonException(DeliveryPolicyErrorCode.ALREADY_EXISTS);
         }
     }
 
+    // 이게 로그인된 멤버의 스토어가 맞나요?
     private void validateStoreOwner(Store store, Member currentMember) {
         if (!store.getMember().getMemberId().equals(currentMember.getMemberId())) {
             throw new CommonException(StoreErrorCode.UNAUTHORIZED_STORE_ACCESS);
         }
     }
 
+    // 상점 주인 검증
     private Store validateAndGetStoreByMember(Member member) {
         return storeRepository
                 .findByMember(member)
                 .orElseThrow(() -> new CommonException(StoreErrorCode.STORE_NOT_FOUND));
+    }
+
+    // ID로 배송 정책 조회
+    private DeliveryPolicy getDeliveryPolicyById(UUID deliveryPolicyId) {
+        return deliveryPolicyRepository
+                .findById(deliveryPolicyId)
+                .orElseThrow(
+                        () ->
+                                new CommonException(
+                                        DeliveryPolicyErrorCode.DELIVERY_POLICY_NOT_FOUND));
     }
 }
