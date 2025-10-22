57f66e842fe4f15232dc736db8ecb1c35249f0dc
isak-kang <wbsldj59@naver.com>
2025-10-22 19:32:26 +0900

    refator #141: errorcode name 수정 NOT_FOUND -> DELIVERY_POLICY_NOT_FOUND

--- src/main/java/com/irum/come2us/global/presentation/advice/exception/errorcode/DeliveryPolicyErrorCode.java
+++ src/main/java/com/irum/come2us/global/presentation/advice/exception/errorcode/DeliveryPolicyErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum DeliveryPolicyErrorCode implements BaseErrorCode {
     ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 해당 상점의 배송비 정책이 존재합니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND, "배송비 정책을 찾을 수 없습니다.");
+    DELIVERY_POLICY_NOT_FOUND(HttpStatus.NOT_FOUND, "배송비 정책을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
