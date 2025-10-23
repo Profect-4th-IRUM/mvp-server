aa83266a89fb06376a67f3ca0d39f43d8ee7acb2
isak-kang <wbsldj59@naver.com>
2025-10-23 14:58:19 +0900

    refactor #139 : memberValidator -> memberUtil 변경

--- src/main/java/com/irum/come2us/domain/deliverypolicy/application/service/DeliveryPolicyService.java
+++ src/main/java/com/irum/come2us/domain/deliverypolicy/application/service/DeliveryPolicyService.java
@@ -5,13 +5,13 @@ import com.irum.come2us.domain.deliverypolicy.domain.repository.DeliveryPolicyRe
 import com.irum.come2us.domain.deliverypolicy.presentation.dto.request.DeliveryPolicyCreateRequest;
 import com.irum.come2us.domain.deliverypolicy.presentation.dto.request.DeliveryPolicyInfoUpdateRequest;
 import com.irum.come2us.domain.deliverypolicy.presentation.dto.response.DeliveryPolicyInfoResponse;
-import com.irum.come2us.domain.member.application.util.MemberValidator;
 import com.irum.come2us.domain.member.domain.entity.Member;
 import com.irum.come2us.domain.store.domain.entity.Store;
 import com.irum.come2us.domain.store.domain.repository.StoreRepository;
 import com.irum.come2us.global.presentation.advice.exception.CommonException;
 import com.irum.come2us.global.presentation.advice.exception.errorcode.DeliveryPolicyErrorCode;
 import com.irum.come2us.global.presentation.advice.exception.errorcode.StoreErrorCode;
+import com.irum.come2us.global.util.MemberUtil;
 import jakarta.transaction.Transactional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -23,11 +23,12 @@ import org.springframework.stereotype.Service;
 public class DeliveryPolicyService {
     private final DeliveryPolicyRepository deliveryPolicyRepository;
     private final StoreRepository storeRepository;
-    private final MemberValidator memberValidator;
+    private final MemberUtil memberUtil;
 
     public UUID createDeliveryPolicy(DeliveryPolicyCreateRequest request) {
-        Member member = memberValidator.getCurrentMember();
+        Member member = memberUtil.getCurrentMember();
         Store store = validateAndGetStoreByMember(member);
+
         ensureStoreHasNoExistingPolicy(store);
 
         DeliveryPolicy deliveryPolicy =
@@ -42,11 +43,7 @@ public class DeliveryPolicyService {
 
     public void changeDeliveryPolicy(
             UUID deliveryPolicyId, DeliveryPolicyInfoUpdateRequest request) {
-        Member member = memberValidator.getCurrentMember();
-        DeliveryPolicy deliveryPolicy = getDeliveryPolicyById(deliveryPolicyId);
-        Store store = validateAndGetStoreByMember(member);
-
-        validateStoreOwner(store, member);
+        DeliveryPolicy deliveryPolicy = validDeliveryPolicy(deliveryPolicyId);
 
         deliveryPolicy.updateFee(request.defaultDeliveryFee());
         deliveryPolicy.updateQuantity(request.minQuantity());
@@ -54,47 +51,45 @@ public class DeliveryPolicyService {
     }
 
     public void withdrawDeliveryPolicy(UUID deliveryPolicyId) {
-        Member member = memberValidator.getCurrentMember();
-        DeliveryPolicy deliveryPolicy = getDeliveryPolicyById(deliveryPolicyId);
+        DeliveryPolicy deliveryPolicy = validDeliveryPolicy(deliveryPolicyId);
 
-        validateStoreOwner(deliveryPolicy.getStore(), member);
         deliveryPolicyRepository.delete(deliveryPolicy);
     }
 
     @Transactional
     public DeliveryPolicyInfoResponse findDeliveryPolicy(UUID deliveryPolicyId) {
-        DeliveryPolicy deliveryPolicy = getDeliveryPolicyById(deliveryPolicyId);
+        DeliveryPolicy deliveryPolicy = validDeliveryPolicy(deliveryPolicyId);
         return DeliveryPolicyInfoResponse.from(deliveryPolicy);
     }
 
-    // 상점 정책 존재하는지 확인
+    // 상점에 배달비 정책이 존재하는지 확인
     private void ensureStoreHasNoExistingPolicy(Store store) {
         if (deliveryPolicyRepository.existsByStore(store)) {
             throw new CommonException(DeliveryPolicyErrorCode.ALREADY_EXISTS);
         }
     }
 
-    // 이게 로그인된 멤버의 스토어가 맞나요?
-    private void validateStoreOwner(Store store, Member currentMember) {
-        if (!store.getMember().getMemberId().equals(currentMember.getMemberId())) {
-            throw new CommonException(StoreErrorCode.UNAUTHORIZED_STORE_ACCESS);
-        }
-    }
-
-    // 상점 주인 검증
+    // 로그인된 멤버의 상점 조회
     private Store validateAndGetStoreByMember(Member member) {
         return storeRepository
                 .findByMember(member)
                 .orElseThrow(() -> new CommonException(StoreErrorCode.STORE_NOT_FOUND));
     }
 
-    // ID로 배송 정책 조회
-    private DeliveryPolicy getDeliveryPolicyById(UUID deliveryPolicyId) {
-        return deliveryPolicyRepository
-                .findById(deliveryPolicyId)
-                .orElseThrow(
-                        () ->
-                                new CommonException(
-                                        DeliveryPolicyErrorCode.DELIVERY_POLICY_NOT_FOUND));
+    // 본인 소유 검증.
+    private DeliveryPolicy validDeliveryPolicy(UUID deliveryPolicyId) {
+        DeliveryPolicy deliverypolicy =
+                deliveryPolicyRepository
+                        .findById(deliveryPolicyId)
+                        .orElseThrow(
+                                () ->
+                                        new CommonException(
+                                                DeliveryPolicyErrorCode.DELIVERY_POLICY_NOT_FOUND));
+        memberUtil.assertMemberResourceAccess(deliverypolicy.getStore().getMember());
+        return deliverypolicy;
     }
 }
+
+// 상점에 이미 정책이 존재하는지 (정책 생성을 위해)
+// 상점의 존재 하는지
+// 본인 소유 검증.
