829143ed6317be163e51cec42e6daaa926ea8df2
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2025-10-26 04:53:11 +0900

    feature #166 : preparePayment 호출 전 결제 취소 시나리오 대응 - 스케줄러

--- src/main/java/com/irum/come2us/domain/order/domain/repository/OrderDetailRepository.java
+++ src/main/java/com/irum/come2us/domain/order/domain/repository/OrderDetailRepository.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,4 +17,9 @@ public interface OrderDetailRepository extends JpaRepository<OrderDetail, UUID> 
     Optional<OrderDetail> findByOrderDetailId(UUID orderDetailId);
 
     List<OrderDetail> findAllByOrder(Order order);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            "UPDATE OrderDetail od SET od.orderStatusIndi = 'FAILED' WHERE od.order.orderId IN :orderIds")
+    int updateStatusToFailedByOrderIds(@Param("orderIds") List<UUID> orderIds);
 }
--- src/main/java/com/irum/come2us/domain/payment/domain/repository/PaymentRepository.java
+++ src/main/java/com/irum/come2us/domain/payment/domain/repository/PaymentRepository.java
@@ -1,9 +1,18 @@
 package com.irum.come2us.domain.payment.domain.repository;
 
 import com.irum.come2us.domain.payment.domain.entity.Payment;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, UUID> {}
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Payment p SET p.paymentStatus = 'FAILED' WHERE p.paymentId IN :paymentIds")
+    int updateStatusToFailedByIds(@Param("paymentIds") List<UUID> paymentIds);
+}
