40951b7cab7cfe8176e82fc2d3b6261ed92d9e94
Wooseok Lee <74820505+Bal1oon@users.noreply.github.com>
2025-10-25 21:08:31 +0900

    [Feature] 상품-카테고리 매핑 (#199)
    * feature #191: 상품 카테고리 생성 및 수정 구현
    
    * feature #191: 카테고리 별 상품 목록 조회 구현
    
    * refactor #191: Spotless 적용
    
    * refactor #191: Spotless 적용
    
    * feature #191: Response 카테고리 필드 추가
    
    * feature #191: 상품 상세 조회 DTO 카테고리 필드 추가
    
    * refactor #191: Spotless 적용

--- src/main/java/com/irum/come2us/global/presentation/advice/exception/errorcode/CategoryErrorCode.java
+++ src/main/java/com/irum/come2us/global/presentation/advice/exception/errorcode/CategoryErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CategoryErrorCode implements BaseErrorCode {
+    INVALID_CATEGORY_DEPTH(HttpStatus.BAD_REQUEST, "상품은 최하위 카테고리에만 속할 수 있습니다."),
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리 정보를 찾을 수 없습니다."),
     CATEGORY_NOT_MODIFIED(HttpStatus.BAD_REQUEST, "카테고리 수정에 대한 변경된 내용이 없습니다."),
     CATEGORY_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 카테고리입니다."),
