d49790d4495a4c195efcc732e45f6a3a9f59a8df
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2025-10-26 03:15:56 +0900

    feature #182 : spotless적용

--- src/main/java/com/irum/come2us/domain/refund/domain/repository/RefundRepository.java
+++ src/main/java/com/irum/come2us/domain/refund/domain/repository/RefundRepository.java
@@ -2,17 +2,18 @@ package com.irum.come2us.domain.refund.domain.repository;
 
 import com.irum.come2us.domain.order.domain.entity.Order;
 import com.irum.come2us.domain.refund.domain.entity.Refund;
+import com.irum.come2us.domain.refund.domain.entity.enums.RefundStatus;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.irum.come2us.domain.refund.domain.entity.enums.RefundStatus;
-import java.util.List;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RefundRepository extends JpaRepository<Refund, UUID> {
     Optional<Refund> findByOrder(Order order);
+
     List<Refund> findByRefundStatus(RefundStatus status);
 
     // RefundRepository 추후 orderRepository에서 가져오는 것으로 변경 예정
