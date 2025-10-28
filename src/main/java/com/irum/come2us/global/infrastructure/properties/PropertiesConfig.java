29b158acb86150d500ce7f3ca2401057f30f713d
psy <psy915@swu.ac.kr>
2025-10-28 13:30:01 +0900

    fix #177: QueryDSL Q클래스 인식 및 Properties 설정 수정
    - build.gradle에 QueryDSL 의존성 버전 명시 (:5.0.0:jakarta)
    - Q클래스 생성 경로(build/generated/querydsl) 인식 추가
    - Lombok annotationProcessor 설정 보완
    - PropertiesConfig 중복 @EnableConfigurationProperties 제거

--- src/main/java/com/irum/come2us/global/infrastructure/properties/PropertiesConfig.java
+++ src/main/java/com/irum/come2us/global/infrastructure/properties/PropertiesConfig.java
@@ -3,7 +3,11 @@ package com.irum.come2us.global.infrastructure.properties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({JwtProperties.class, RedisProperties.class, FileProperties.class})
-@EnableConfigurationProperties({JwtProperties.class, RedisProperties.class, TossProperties.class})
 @Configuration
+@EnableConfigurationProperties({
+    JwtProperties.class,
+    RedisProperties.class,
+    TossProperties.class,
+    FileProperties.class
+})
 public class PropertiesConfig {}
