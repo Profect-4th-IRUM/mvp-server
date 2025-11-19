32c640126ec97743641d612f4406887863f33f37
willjsw <sunwoo1137@gmail.com>
2025-11-19 19:25:33 +0900

    test #246: stock-quantity 증량

--- src/main/resources/db/changelog/product-changes/dml/024-insert-default-product_option_groups_and_values.sql
+++ src/main/resources/db/changelog/product-changes/dml/024-insert-default-product_option_groups_and_values.sql
@@ -48,7 +48,7 @@ SELECT
         )::uuid AS option_value_id,
         option_group_id,
     'M' AS name,
-    100 AS stock_quantity,
+    100000 AS stock_quantity,
     0 AS extra_price,
     CURRENT_TIMESTAMP,
     '100000',
